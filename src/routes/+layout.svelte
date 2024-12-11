11cf74729131fe4d543efeb5bb50511559719203
mattdevtx <matt@mattrodriguez.tech>
2024-12-10 20:50:02 -0600

    add styles to nav elemnts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 <nav>
 	<div class="logo">
 		<!-- svelte-ignore a11y_missing_attribute -->
-		<img src={Logo} class="logo__image"/>
-    <div class="logo__text">Steady Wins Design</div>
+		<img src={Logo} class="logo__image" />
+		<div class="logo__text">Steady Wins Design</div>
 	</div>
 	<ul>
 		<li><a href="/">Home</a></li>
@@ -17,35 +17,44 @@
 <slot />
 
 <style>
-  nav {
-    display: flex;
-    justify-content: space-between;
-    padding: 0 3rem;
-  }
+	nav {
+		display: flex;
+		justify-content: space-between;
+		padding: 1rem 4rem;
+    align-items: flex-end;
+	}
 
-  .logo {
-    display: flex;
-    align-items: end;
-  }
+	.logo {
+		display: flex;
+		align-items: end;
+	}
 
-  .logo__image {
-    width: 112px;
-    margin-right: 1rem;
-  }
+	.logo__image {
+		width: 112px;
+		margin-right: 1rem;
+	}
 
-  .logo__text {
-    font-size: 2.25rem;
-    font-weight: 600;
-  }
+	.logo__text {
+		font-size: 2.25rem;
+		font-weight: 500;
+	}
 
 	ul {
 		display: flex;
 		list-style: none;
+    margin-bottom: 0.25rem;
 	}
 
 	li {
-		margin-left: 1.5rem;
+		font-size: 1.25rem;
+		font-weight: 500;
+		margin-left: 1.75rem;
+    transition: all 0.3s linear;
 	}
+
+  li:hover {
+    transform: scale(1.1);
+  }
 
 	a {
 		text-decoration: none;
